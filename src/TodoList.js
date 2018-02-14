e3525d529f5d30aa196b483f2eb6eb006a50524f
unknown <test@adami3.com>
2018-02-15 00:19:10 +0100

    display todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,19 +1,48 @@
 import React, { Component } from "react";
+import TodoItems from "./TodoItems";
 
 class TodoList extends Component {
-  render() {
-    return (
-      <div className="todoListMain">
-        <div className="header">
-          <form>
-            <input placeholder="enter task">
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            items: []
+        };
+        this.addItem = this.addItem.bind(this);
+    }
+    addItem(e) {
+        var itemArray = this.state.items;
+
+        if (this._inputElement.value !== "") {
+            itemArray.unshift({
+                text: this._inputElement.value,
+                key: Date.now()
+            });
+
+            this.setState({
+                items: itemArray
+            });
+
+            this._inputElement.value = "";
+        }
+
+        console.log(itemArray);
+
+        e.preventDefault();
+    }
+    render() {
+        return (
+            <div className="todoListMain">
+            <div className="header">
+            <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputElement = a} placeholder="enter task">
             </input>
             <button type="submit">add</button>
-          </form>
-        </div>
-      </div>
-    );
-  }
+            </form>
+            </div>
+            <TodoItems entries={this.state.items}/>
+            </div>
+        );
+    }
 }
 
 export default TodoList;
